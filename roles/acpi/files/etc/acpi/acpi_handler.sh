30f28e99aadda69eafbb7d1e23b6505fe70e382a
Chris Rendle-Short <chris@killred.net>
2016-05-22 19:24:12 +1000

    Add basic caffeination support.

--- roles/acpi/files/etc/acpi/acpi_handler.sh
+++ roles/acpi/files/etc/acpi/acpi_handler.sh
@@ -5,6 +5,7 @@
 lid="/proc/acpi/button/lid/C155"
 
 
+# Run things as the current X user. Necessary for screen locker.
 function runasXuser {
 	Xtty=$(</sys/class/tty/tty0/active)
 	Xpid=$(pgrep -t $Xtty -f /usr/bin/X)
@@ -22,31 +23,46 @@ function runasXuser {
 }   
 
 
+function lock_screen {
+	pkill xlock
+	runasXuser "/usr/bin/xlock -nice 19 -mode flame -dpmsoff 60 -background black -foreground white &" 2>/dev/null
+}
+
+
+# Check for caffeinated state.
+# RC0 = caffeinated
+function caffeinated {
+	caffeinate=$(which caffeinate 2>&1) || return 1
+	$caffeinate -c >/dev/null 2>&1 && return 0
+	return 1
+}
+
+
 # Run this before suspend/hibernate
 function freeze {
 	pkill mbsync
 	/sbin/umount -l /mnt/sd
 	/usr/sbin/alsactl store
 	/sbin/hwclock --systohc
-	pkill xlock
-	runasXuser "/usr/bin/xlock -nice 19 -mode flame -dpmsoff 60 -background black -foreground white &" 2>/dev/null
+	lock_screen
 }
 
 
-function do_suspend {
-	ps auxf > /root/suspend.ps.log
-	/usr/sbin/pm-suspend
-}
+# Trigger sleep
+# Arg 1 - mode ('suspend' or 'hibernate')
+function do_sleep {
+	local mode="$1"
 
-
-function do_hibernate {
-	ps auxf > /root/hibernate.ps.log
-	/usr/sbin/pm-hibernate
+	freeze
+	ps auxf > /root/${mode}.ps.log
+	/usr/sbin/pm-${mode}
+	thaw
 }
 
 
 # Run this after resuming from suspend/hibernate
 function thaw {
+	caffeinate 0 >/dev/null 2>&1
 	/sbin/hwclock --hctosys
 	/usr/sbin/alsactl restore
 
@@ -63,9 +79,11 @@ case "$1" in
 		;;
 	'button/lid')
 		if grep '^state:\s\+closed$' ${lid}/state >/dev/null; then
-			freeze
-			do_suspend
-			thaw
+			if caffeinated; then
+				lock_screen
+			else
+				do_sleep suspend
+			fi
 		fi
 		;;
 esac
