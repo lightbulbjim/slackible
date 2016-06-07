b14cb61a82e952f36150813af0d5aeabba668f89
Chris Rendle-Short <chris@killred.net>
2016-06-07 18:35:46 +1000

    Make volume script more flexible.

--- roles/acpi/files/etc/acpi/volume.sh
+++ roles/acpi/files/etc/acpi/volume.sh
@@ -1,24 +1,41 @@
-#/bin/sh
+#/bin/bash
 # Managed by Ansible
 # Volume control helper script
 
-sink='@DEFAULT_SINK@'
-source='@DEFAULT_SOURCE@'
 step=5
+use_amixer=0
+sink='@DEFAULT_SINK@'
+src='@DEFAULT_SOURCE@'
 
 case $1 in
 	mute)
-		pactl set-sink-mute $sink toggle
+		if [[ "$use_amixer" -eq 1 ]]; then
+			amixer -D pulse sset Master toggle
+		else
+			pactl set-sink-mute $sink toggle
+		fi
 		;;
 	down)
-		pactl set-sink-mute $sink 0
-		pactl set-sink-volume $sink -${step}%
+		if [[ "$use_amixer" -eq 1 ]]; then
+			amixer -D pulse sset Master unmute ${step}%-
+		else
+			pactl set-sink-mute $sink 0
+			pactl set-sink-volume $sink -${step}%
+		fi
 		;;
 	up)
-		pactl set-sink-mute $sink 0
-		pactl set-sink-volume $sink +${step}%
+		if [[ "$use_amixer" -eq 1 ]]; then
+			amixer -D pulse sset Master unmute ${step}%+
+		else
+			pactl set-sink-mute $sink 0
+			pactl set-sink-volume $sink +${step}%
+		fi
 		;;
 	micmute)
-		pactl set-source-mute $source toggle
+		if [[ "$use_amixer" -eq 1 ]]; then
+			amixer -D pulse sset Capture toggle
+		else
+			pactl set-source-mute $src toggle
+		fi
 		;;
 esac
